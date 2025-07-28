8cee1762e5b4b8bc2e39f753e26075ebe35f5724
Alex Tofan <7130188+savaryna@users.noreply.github.com>
2025-07-28 14:36:23 +0200

    add dark theme to demo and split into components
    - optimize fonts
    - add astro check before build
    - use a base layout for the page

--- packages/demo/tailwind.config.mjs
+++ packages/demo/tailwind.config.mjs
@@ -3,6 +3,7 @@ import materialSymbols from '@savaryna/tailwindcss-material-symbols';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   safelist: [
     {
